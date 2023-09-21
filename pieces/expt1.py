0417d4b292b0e0e43dbc6deff938048b686ef5f2
Bruce Lucas <bdlucas1@gmail.com>
2023-09-21 09:03:22 -0400

    Update expt1.py

--- pieces/expt1.py
+++ pieces/expt1.py
@@ -1,12 +1,11 @@
 import notation
 
-# rhythmic (accent) patterns apply to succeeding notes
-R121 = I > [(1/4,8), (2/4,8)] # quarter note vol +8, then half note vol +8
-R4 = I > [(4/4,8)]            # whole note vol +8
+# rhythmic (accent) patterns apply to succeeding notes from start of bar (I)
+I121 = I > [(1/4,8), (2/4,8)] # quarter note vol +8, then half note vol +8
+I4 = I > [(4/4,8)]            # whole note vol +8
 
-# define a portamento of s semitones for the last x of a note of length t
-def port(t, s=-2, x=1/16):
-    return [(t-x,0),(x,0,s)]
+# portamento of -2 semitones for the last 1/16 of a 1/2 note
+port = [(1/2-1/16, 0), (1/16, 0, -2)]
 
 P(
     guitar,
@@ -15,8 +14,8 @@ P(
     transpose(-12),
 
     S(
-        R121, a4/4, d/2@port(1/2), b/4, R4, c/1,
-        R121, a4/4, d/2          , b/4, R4, c/1,
+        I121, a4/4, d/2@port, b/4, I4, c/1,
+        I121, a4/4, d/2     , b/4, I4, c/1,
     )
 
 ).write().play()
