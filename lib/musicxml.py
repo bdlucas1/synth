b5dc34c1ba4531ecb789ba54327dc6f4f65a9501
Bruce Lucas <bdlucas1@gmail.com>
2023-10-16 11:04:38 -0400

    Bar checks
    and tempo dots

--- lib/musicxml.py
+++ lib/musicxml.py
@@ -26,7 +26,7 @@ def read(fn):
     for measure in root.findall(".//measure"):
     
         for item in measure:
-    
+            
             if item.tag == "attributes":
 
                 # xxx does attributes always require this?
@@ -37,23 +37,25 @@ def read(fn):
                 d = item.find("time/beat-type")
                 if n is not None and d is not None:
                     main.append(notation.time(int(n.text), int(d.text)))
-    
+                    
                 # transpose
                 t = item.find("transpose/chromatic")
                 if t is not None:
                     main.append(notation.transpose(int(t.text)))
-                
+                    
                 # divisions
                 d = item.find("divisions")
                 if d is not None: divisions = int(d.text)
 
             elif item.tag == "direction":
-            
+                
                 beat_unit = item.find("direction-type/metronome/beat-unit")
                 per_minute = item.find("direction-type/metronome/per-minute")
                 if beat_unit is not None and per_minute is not None:
                     beat_units = {"sixteenth":16, "eighth":8, "quarter":4, "half":2, "whole":1}
                     beat_unit = beat_units[beat_unit.text]
+                    if item.find("direction-type/metronome/beat-unit-dot") is not None:
+                        beat_unit *= 2/3
                     main.append(notation.tempo(beat_unit, int(per_minute.text)))
 
             elif item.tag == "barline":
@@ -65,38 +67,40 @@ def read(fn):
                     end_segment()
 
             elif item.tag == "note":
-    
+                
                 # voice
                 voice = item.find("voice")
                 if voice is not None: voice = int(voice.text)
-        
+                
                 # pitch
                 pitch = item.find("pitch/step").text.lower() + item.find("pitch/octave").text
-                pitch = getattr(builtins, pitch).pitch # provided by notations
-        
+                alter = item.find("pitch/alter")
+                alter = int(alter.text) if alter is not None else 0
+                pitch = getattr(builtins, pitch).pitch + alter# provided by notations
+                
                 # duration
                 dur_divisions = int(item.find("duration").text)
                 dur = notation.T(dur_divisions, divisions = divisions)
-    
+                
                 # atom
                 atom = notation.Atom(pitch = pitch, dur = dur)
-    
+                
                 # chord?
                 is_chord = item.find("chord") is not None
                 
-                def emit(voice, atom):
-                    while len(segment) < voice:
+                def emit(voice_number, atom):
+                    while len(segment) < voice_number:
                         segment.append(S())
-                    voice = segment[voice-1]
+                    voice_segment = segment[voice_number-1]
                     if is_chord:
-                        last = voice[-1]
+                        last = voice_segment[-1]
                         if isinstance(last, notation.P):
                             last.append(atom)
                         else:
                             chord = P(last, atom)
-                            voice[-1] = chord
+                            voice_segment[-1] = chord
                     else:
-                        voice.append(atom)
+                        voice_segment.append(atom)
 
                 # emit
                 if voice is not None:
@@ -106,7 +110,10 @@ def read(fn):
                     for voice in range(2, len(segment)+1):
                         # xxx could just extend last rest
                         emit(voice, notation.Atom(pitch = "rest", dur = dur))
-    
+
+        # bar check at end of each measure
+        for voice_segment in segment:
+            voice_segment.append(I)
 
     end_segment()
     notation.Items.main = main
